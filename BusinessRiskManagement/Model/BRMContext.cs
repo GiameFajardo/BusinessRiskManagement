7d858818d54e3fcae828e4ba954d11097441cffd
Giame Fajardo <giame_fajardo@cecomsa.com>
2021-06-12 20:43:39 -0400

    Bd init

---
+++ BusinessRiskManagement/Model/BRMContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessRiskManagement.Model
+{
+    public class BRMContext : DbContext
+    {
+        public BRMContext(DbContextOptions<BRMContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Individual>();
+            modelBuilder.Entity<Company>();
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public DbSet<Organizacion> Organizacions { get; set; }
+    }
+}
---
+++ BusinessRiskManagement/Startup.cs
@@ -0,0 +1,84 @@
+using BusinessRiskManagement.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BusinessRiskManagement
+{
+    public class Startup
+    {
+
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            if (_env.IsStaging())
+            {
+                services.AddDbContext<BRMContext>(option =>
+                    option.UseSqlServer(Configuration.GetConnectionString("StagingConnection")));
+            }
+            else if (_env.IsEnvironment("Quality&Assurance"))
+            {
+                services.AddDbContext<BRMContext>(option =>
+                    option.UseSqlServer(Configuration.GetConnectionString("Quality&AssuranceConnection")));
+            }
+            else if (_env.IsDevelopment())
+            {
+                services.AddDbContext<BRMContext>(option =>
+                    option.UseSqlServer(Configuration.GetConnectionString("DevelopmentConnection")));
+            }
+            else
+            {
+                services.AddDbContext<BRMContext>(option =>
+                    option.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
+            }
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
